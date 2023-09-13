a51c3f34c99a3c76a8187809d3abcc4d40394047
selenemulmi <selenemulmi1234@gmail.com>
2023-09-13 10:57:17 +0545

    Simple Fitnessapp using html, css and js

---
+++ script.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function(){
+
+    $('.buttons').click(function(){
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+        var filter = $(this).attr('data-filter')
+
+        if(filter == 'all'){
+            $('.diet .box').show(400);
+        }
+        else{
+            $('.diet .box').not('.'+ filter).hide(200);
+            $('.diet .box').filter('.'+ filter).show(400);
+        }
+
+    });
+
+});
+
+
+var x=document.getElementById('login-form');
+var y=document.getElementById('register');
+var z=document.getElementById('btn');
+function register()
+{
+    x.style.left='-400px';
+    y.style.left='50px';
+    z.style.left='110px';
+}
+function login()
+{
+    x.style.left='50px';
+    y.style.left='450px';
+    z.style.left='0px';
+}
+
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        modal.style.display = "none";
+    }
+}
+
+var swiper = new Swiper('.review-slider', {
+    loop: true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        640:{
+            slidesPerView: 2,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+    },
+})
